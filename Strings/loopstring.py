fff79fa0d2d71892342d4d747c7a9ba7b48ab375
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-05-27 00:52:09 +0530

    Add files via upload

---
+++ Strings/loopstring.py
@@ -0,0 +1,12 @@
+name = 'sanjiv'
+index = 0
+# looping through strings - indeterminate loop [while]
+print('indeterminate loop:')
+while index < len(name):
+    letter = name[index]
+    print(index, letter)
+    index += 1
+# looping through strings - determinate loop [for]
+print('determinate loop:')
+for i in name:
+    print(i)
