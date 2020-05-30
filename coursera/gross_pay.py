5984b3d6ae0c5e41bb4220e6ccfffe085b10937d
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-05-30 23:48:55 +0530

    Update gross_pay.py

--- coursera/gross_pay.py
+++ coursera/gross_pay.py
@@ -7,10 +7,10 @@ try:
 	if hour < 0:
 		print("Please enter a valid number") # error message
 	elif rate < 0:
-		print("Please enter a valid number")
+		print("Please enter a valid number") # error message
 	elif hour > 40:
-		print("%.2f" % (40*rate+(hour-40)*1.5*rate))
+		print("%.2f" % (40*rate+(hour-40)*1.5*rate)) # Equation for Overtime usage
 	else:
-		print("%.2f" % (hour*rate))
+		print("%.2f" % (hour*rate)) # Equation for Normal usage
 except:
-		print("Please enter a valid number")
+		print("Please enter a valid number") # Error message
