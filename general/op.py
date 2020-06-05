18580ad73338cbd5e8462b2307d318958ca3e9fa
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-06 01:24:08 +0530

    Update op.py

--- general/op.py
+++ general/op.py
@@ -1,6 +1,6 @@
 # function to add
 def operate(a,b):
     add = a + b 
-    return add # return value
-x = operate(3,4)
-print(x)
+    return add # return sum
+x = operate(3,4) # invoke function
+print(x) # displaying the sum
