bfde587eb7cdde50c5d2d60fc88eeb1aed79e609
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-07 01:10:44 +0530

    Update addfunc.py

--- general/addfunc.py
+++ general/addfunc.py
@@ -1,7 +1,7 @@
 # Defining a function to add
 def add(a,b):
     return a+b # returning sum
-
+# User input
 a = int(input('Enter first operand: ')) 
 b = int(input('Enter first operand: '))
 c = add(a,b) 
