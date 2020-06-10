f3f2071a3d9e45e1297aba469d9e850d6ffe9946
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-11 00:09:46 +0530

    Update listloopappendavg.py

--- Lists/listloopappendavg.py
+++ Lists/listloopappendavg.py
@@ -1,6 +1,7 @@
-numlist = list()
+# Computing average with inbuilt functions
+numlist = list() 
 while True:
-    inp = input('Enter a number: ')
+    inp = input('Enter a number: ') # User inout
     if inp == 'done' :  break
     value = float(inp)
     numlist.append(value)
