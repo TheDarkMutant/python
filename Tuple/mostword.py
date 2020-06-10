0b5aaf74633c3c60ee3c510fb24a3ba5423f8f5d
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-10 20:07:57 +0530

    Update mostword.py

--- Tuple/mostword.py
+++ Tuple/mostword.py
@@ -4,11 +4,11 @@ counts = dict()
 for line in fhand: # Iterate through every line of a file
     words = line.split()
     for word in words: # frequency of words
-        counts[word] = counts.get(word,0) + 1 # 
+        counts[word] = counts.get(word,0) + 1 # Increment count on encounter 
 lst = list()
 for k,v in counts.items():
     newtup = (v,k)
-    lst.append(newtup)
+    lst.append(newtup) 
 lst = sorted(lst, reverse=True)
 for v,k in lst[:10]:
     print(k,v)
