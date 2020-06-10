792da4cc402326148ef9a831e9f8c3fd9805d065
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-10 20:01:49 +0530

    Update listloopavg.py

--- Lists/listloopavg.py
+++ Lists/listloopavg.py
@@ -1,12 +1,13 @@
 # To determine average of numbers
 total = 0
 count = 0
-while True:
+while True: #indeterminate loop
+    # user input
     inp = input('Enter a number: ')
     if inp == 'done' : break
     value = float(inp)
     total = total + value
     count = count + 1
-
+# computing average
 average = total / count
-print('Average: ', average)
+print('Average: ', average) 
