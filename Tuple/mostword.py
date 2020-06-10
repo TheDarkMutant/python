626f827eb85e651f6670d907c96e08140a21ee9a
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-10 20:06:07 +0530

    Update mostword.py

--- Tuple/mostword.py
+++ Tuple/mostword.py
@@ -1,10 +1,10 @@
 # To determine words used the most in a file
 fhand = open('romeo.txt') # file open
 counts = dict()
-for line in fhand:
+for line in fhand: # Iterate through every line of a file
     words = line.split()
-    for word in words:
-        counts[word] = counts.get(word,0) + 1
+    for word in words: # frequency of words
+        counts[word] = counts.get(word,0) + 1 # 
 lst = list()
 for k,v in counts.items():
     newtup = (v,k)
