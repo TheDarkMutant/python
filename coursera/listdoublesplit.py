58e9b5c3370552912e97c3a8ccb221425c77a416
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-14 08:43:13 +0530

    Update listdoublesplit.py

--- coursera/listdoublesplit.py
+++ coursera/listdoublesplit.py
@@ -1,12 +1,12 @@
 # Illustraion of double split of list
-fname = input("Enter file name: ") 
+fname = input("Enter file name: ") # User Input
 if len(fname) < 1 : fname = "mbox-short.txt"
 
-fh = open(fname)
+fh = open(fname) # file open
 count = 0
 l = [i.split() for i in fh.readlines()
-            if i.startswith("From") and i.find("@")>0 and len(i.split())>2]
+            if i.startswith("From") and i.find("@")>0 and len(i.split())>2] # split condition
 for i in l:
-    print(i[1])
+    print(i[1]) 
     count+=1
 print("There were", count, "lines in the file with *From* as the first word")
