d98fd21a0bed1a1579bf7d41cd46c99531a16c78
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-14 08:32:19 +0530

    Update listssort.py

--- coursera/listssort.py
+++ coursera/listssort.py
@@ -6,11 +6,11 @@ fh = open(fname)
 lst = list()
 # Iterates through each line of the file
 for line in fh:
-    #Iterates through each word in line
+    # Iterates through each word in line
     for i in line.split():
-        #Checks for the word repitition in file
+        # Checks for the word repitition in file
         if not i in lst:
-            #Appends words to list
+            # Appends words to list
             lst.append(i)
 
 lst.sort()
