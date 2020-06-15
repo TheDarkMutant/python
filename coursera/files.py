64ce1681ed44ce56b4eedfeb2614b2cd964ce70c
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-15 22:34:59 +0530

    Update files.py

--- coursera/files.py
+++ coursera/files.py
@@ -1,6 +1,6 @@
 # Accessing and manipulaating files
-fname = input("Enter the file name: ")
-f = open(fname)
+fname = input("Enter the file name: ") # user input
+f = open(fname) # file open
 for i in f:
     i = i.rstrip().upper()
     print(i)
