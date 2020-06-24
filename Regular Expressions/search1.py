587ed70bdd9a2727644ea64bc9d049108f4adf2a
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-24 20:57:20 +0530

    Add files via upload

---
+++ Regular Expressions/search1.py
@@ -0,0 +1,12 @@
+import re
+
+fhand = open('mbox-short.txt')
+
+# 1. search()
+for line in fhand:
+    line = line.rstrip()
+    # Find 'From:' at the beginning of line
+    # ^: beginning of
+    # True/false if you find or not
+    if re.search('^From:', line):
+        print(line)
