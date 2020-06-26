704775f7faaee451b52d31f036ce962754f50507
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-27 00:29:57 +0530

    Add files via upload

---
+++ Regular Expressions/extract3.py
@@ -0,0 +1,11 @@
+import re
+
+fhand = open('mbox-short.txt')
+
+line = 'From stephen@u.nus.edu do not'
+# ^From --> starts From
+# .* --> any character before @
+# ( ) --> what you will get
+# [^ ]* --> non-blank characters as many as them
+c = re.findall('^From .*@([^ ]*)', line)
+print(c)
