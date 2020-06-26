4a65f969f598882d76dc7adff485a86673b86770
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-27 00:31:29 +0530

    Add files via upload

---
+++ Regular Expressions/search2.py
@@ -0,0 +1,11 @@
+import re
+
+fhand = open('mbox-short.txt')
+
+# 1. search()
+for line in fhand:
+    line = line.rstrip()
+    # * 0 or more digits, any number of times
+    # . matches any character
+    if re.search('^X.*:', line):
+        print(line)
