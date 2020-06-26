57e5f586e0978a36974909768131b247d7a7b504
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-27 00:31:54 +0530

    Add files via upload

---
+++ Regular Expressions/search3.py
@@ -0,0 +1,12 @@
+import re
+
+fhand = open('mbox-short.txt')
+
+# 1. search()
+for line in fhand:
+    line = line.rstrip()
+    # \S --> non-blank character
+    # + --> one or more times
+    # This basically searches for X- with non-blank characters up to :
+    if re.search('^X-\S+:', line):
+        print(line)
