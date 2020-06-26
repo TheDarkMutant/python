1192ca5a9ba99a7c184039017311ab651ea0e1a0
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-27 00:32:13 +0530

    Add files via upload

---
+++ Regular Expressions/Spamconfidence.py
@@ -0,0 +1,16 @@
+import re
+
+fhand = open('mbox-short.txt')
+
+# SPAM CONFIDENCE Example
+numlist = list()
+for line in fhand:
+    line = line.rstrip()
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    # skip those you don't find
+    print(stuff)
+    if len(stuff) != 1:
+        continue
+    num = float(stuff[0])
+    numlist.append(num)
+print('Maximium:', max(numlist))
