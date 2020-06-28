fbd099add1072edfde4463d78c91600f11671e3b
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-06-28 22:21:51 +0530

    Update extract2.py

--- Regular Expressions/extract2.py
+++ Regular Expressions/extract2.py
@@ -6,7 +6,7 @@ fhand = open('mbox-short.txt')
 # Extracting only the domain
 line = 'From stephen@u.nus.edu do not'
 # find @ in line
-# ( ) gives you what you're looking for
+# ( ) give you what you're looking for
 # [^ ] non blank
 # * 0 or more
 b = re.findall('@([^ ]*)', line)
