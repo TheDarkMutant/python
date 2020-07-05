7d556dedc85a81b0708eeb172c700c4ec5371d06
TheDarkMutant <48614619+TheDarkMutant@users.noreply.github.com>
2020-07-05 23:56:23 +0530

    Update findall.py

--- Regular Expressions/findall.py
+++ Regular Expressions/findall.py
@@ -3,6 +3,7 @@ import re
 # opening the file
 fhand = open('mbox-short.txt')
 # findall()
+# extracting the numbers in the line
 x = 'my 2 favourite numbers are 19 and 24'
 # [0-9] digits and 1 or more digit
 y = re.findall('[0-9]+', x)
